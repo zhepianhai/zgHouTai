af0ad33673dba634cec984236999fe9a1c18c82f
zhepianhai <1364778591@qq.com>
2021-09-11 15:24:45 +0800

    Initial commit

---
+++ src/main/java/com/zph/zg/ZgApplication.java
@@ -0,0 +1,13 @@
+package com.zph.zg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zph/zg/ZgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zph.zg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
